ae13a35756342ec309f5c9c9eff56f2c3fb77048
sirnitro <54961208+sirnitro@users.noreply.github.com>
2023-07-15 13:23:19 -0400

    Update pump_controller.ino
    Cleaned code up and added comments

--- pump_controller.ino
+++ pump_controller.ino
@@ -1,33 +1,27 @@
 const unsigned long onDurationMs = 900000; // 15 Minutes
-//const unsigned long onDurationMs = 10000; // 10 Seconds
-//unsigned long onDurationSecs = onDurationMs/1000;
-//unsigned long onDurationMins = onDurationSecs/60;
-
 const unsigned long offDurationMs = 10800000; // 3 Hours
-//const unsigned long offDurationMs = 15000; // 15 Seconds
-//unsigned long offDurationSecs = offDurationMs/1000;
-//unsigned long offDurationMins = offDurationSecs/60;
-
-unsigned long onTimeMs = 0;
-unsigned long offTimeMs = 0;
-int pumpState = LOW;
+unsigned long onTimeMs = 0; // Used to track how long the pump has been running
+unsigned long offTimeMs = 0; //Used to track how long the pump has been off
+int pumpState = LOW; //Set the pump to off to begin with
 int relay = 12; // Plug the relay into Digital Pin 12
 
 void setup(){
-  Serial.begin(9600);
-  pinMode(relay, OUTPUT);
+  Serial.begin(9600); //Serial Output for debugging
+  pinMode(relay, OUTPUT); 
 }
 
 void loop(){
  if( pumpState ==LOW ){ 
    if( (millis()-offTimeMs) >= offDurationMs){
-      pumpState = HIGH;
-      onTimeMs=millis();
-      
+      pumpState = HIGH; // Turn Pump On
+      onTimeMs=millis(); // Set time that pump was turned on
+      digitalWrite(LED_BUILTIN,HIGH); // Turn Board LED On
+      digitalWrite(relay, HIGH); // Turn the relay on
+
+// BEGIN SERIAL DEBUG INFORMATION
       unsigned long onTimeSecs = onTimeMs/1000;
       unsigned long onTimeMins = onTimeSecs/60;
       unsigned long onTimeHrs = onTimeMins/60;
-      
       Serial.print(millis());
       Serial.print(":");
       Serial.print(onTimeSecs);
@@ -35,19 +29,23 @@ void loop(){
       Serial.print(onTimeMins);
       Serial.print(":");
       Serial.print(onTimeHrs);
-      Serial.println("  Pump On");
-      
-      digitalWrite(LED_BUILTIN,pumpState);
-      digitalWrite(relay, HIGH); // Turn the relay on
+      Serial.println(Pump On");
+// END SERIAL DEBUG INFORMATION
+     
+
    }
  }else{   
     if( (millis()-onTimeMs) >= onDurationMs){
-      pumpState = LOW;
-      offTimeMs=millis();
+      pumpState = LOW; //Turn Pump off
+      offTimeMs=millis(); //Set time Pump went off
+      digitalWrite(LED_BUILTIN,HIGH); // Turn Board LED Off
+      digitalWrite(relay, LOW); // Turn the relay Off
+      
+      
+// BEGIN SERIAL DEBUG INFORMATION
       unsigned long offTimeSecs = offTimeMs/1000;
       unsigned long offTimeMins = offTimeSecs/60;
       unsigned long offTimeHrs = offTimeMins/60;
-      
       Serial.print(millis());
             Serial.print(":");
       Serial.print(offTimeSecs);
@@ -56,9 +54,9 @@ void loop(){
             Serial.print(":");
       Serial.print(offTimeHrs);
       Serial.println("  Pump Off");
+// END SERIAL DEBUG INFORMATION
       
-      digitalWrite(LED_BUILTIN,pumpState);
-      digitalWrite(relay, LOW); // Turn the relay Off
+
     }
  }
    
